6ae47cd84766e715c95906afb4c937752d46ef94
Akash N Gowda <151770358+kashh004@users.noreply.github.com>
2024-10-05 13:19:08 +0530

    Create  Binary Search.c

---
+++ Languages/Principles of Programming using C/Binary Search.c
@@ -0,0 +1,40 @@
+// Question 5: Implement Binary Search on Integers.
+
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int key) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key)
+            return mid;
+        if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int n, key, result;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the key to search: ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, 0, n - 1, key);
+    
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
